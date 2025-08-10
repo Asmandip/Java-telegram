3c961943f048233ea82e887a2199a1bc78fe26e9
Asmandip <asmandip@gmail.com>
2025-08-10 20:00:37 +0530

    PnL.js

--- models/PnL.js
+++ models/PnL.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-const PnlSchema = new mongoose.Schema({
-  tradeId: { type: mongoose.Schema.Types.ObjectId },
-  pair: String,
-  entry: Number,
-  exit: Number,
-  pnl: Number,
-  createdAt: { type: Date, default: Date.now }
+const Schema = new mongoose.Schema({
+  tradeId: mongoose.Schema.Types.ObjectId, pair: String, entry: Number, exit: Number, pnl: Number, createdAt: { type:Date, default: Date.now }
 });
-module.exports = mongoose.model('PnL', PnlSchema);
+module.exports = mongoose.model('PnL', Schema);
