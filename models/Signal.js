fed026ae8ff7473c926c1f2d43802f2afb45f4fa
Asmandip <asmandip@gmail.com>
2025-08-10 19:59:20 +0530

    Signal.js

--- models/Signal.js
+++ models/Signal.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
-const SignalSchema = new mongoose.Schema({
-  pair: String,
-  symbol: String,
-  type: String,
-  price: Number,
-  confirmations: [String],
-  indicators: Object,
-  status: { type: String, default: 'candidate' },
-  createdAt: { type: Date, default: Date.now },
-  executedAt: Date,
-  execResult: Object
+const Schema = new mongoose.Schema({
+  pair: String, symbol: String, type: String, price: Number,
+  confirmations: [String], indicators: Object,
+  status: { type:String, default:'candidate' },
+  createdAt: { type: Date, default: Date.now }, executedAt: Date, execResult: Object
 });
-module.exports = mongoose.model('Signal', SignalSchema);
+module.exports = mongoose.model('Signal', Schema);
